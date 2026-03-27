8e80a45c6405493bfab4f37e22cc9dfe80d2178d
RYUseless <janfilipzacek@seznam.cz>
2026-03-27 14:03:38 +0100

    sync

---
+++ Ryu's-Chatting-Application/app/src/main/java/ryu/masters_thesis/P2P_ChattingApp/MainActivity.kt
@@ -0,0 +1,47 @@
+package ryu.masters_thesis.P2P_ChattingApp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ryu.masters_thesis.P2P_ChattingApp.ui.theme.RyusChattingApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RyusChattingApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RyusChattingApplicationTheme {
+        Greeting("Android")
+    }
+}
