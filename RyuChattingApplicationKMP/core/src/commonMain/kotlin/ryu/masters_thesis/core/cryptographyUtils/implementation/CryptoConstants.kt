054d6494a9242d6c866173c3914c6595ddebb346
RYUseless <janfilipzacek@seznam.cz>
2026-05-26 00:31:09 +0200

    Did not sync this

--- RyuChattingApplicationKMP/core/src/commonMain/kotlin/ryu/masters_thesis/core/cryptographyUtils/implementation/CryptoConstants.kt
+++ RyuChattingApplicationKMP/core/src/commonMain/kotlin/ryu/masters_thesis/core/cryptographyUtils/implementation/CryptoConstants.kt
@@ -4,7 +4,7 @@ internal object CryptoConstants {
     const val AES_KEY_SIZE        = 256
     const val IV_SIZE             = 16
     const val SALT_SIZE           = 16
-    const val PBKDF2_ITERATIONS   = 50_000
+    const val PBKDF2_ITERATIONS   = 600_000
     const val AES_ALGORITHM       = "AES"
     const val AES_CIPHER          = "AES/GCM/NoPadding"
     const val PBKDF2_ALGORITHM    = "PBKDF2WithHmacSHA256"
